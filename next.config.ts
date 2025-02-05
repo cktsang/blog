5859a16d96be91cc74275c1b883912f797239369
cktsang <ck.tsang@hotmail.com>
2025-02-05 04:02:21 +0100

    Updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  ignoreDuringBuilds: true,
   experimental: {
     scrollRestoration: true,
   },
