85009dd73d848381248e75ab85434c69b5ff72a3
cktsang <ck.tsang@hotmail.com>
2025-02-11 23:59:21 +0100

    changed showmore

--- components/posts/PostList.tsx
+++ components/posts/PostList.tsx
@@ -255,7 +255,7 @@ function DateComponent(date: { data: string }) {
 
 function ArticleBody(props: any) {
   const ref = useRef<HTMLDivElement | null>(null!);
-  const [showMore, setShowMore] = useState(true);
+  const [showMore, setShowMore] = useState(false);
 
   useEffect(() => {
     if (!ref.current) return;
@@ -263,7 +263,7 @@ function ArticleBody(props: any) {
       const resizeObserver = new ResizeObserver(() => {
         const height = ref.current?.getBoundingClientRect().height;
 
-        height && setShowMore(height > 190);
+        height && height > 175 ? setShowMore((showMore) => !showMore) : null;
       });
 
       resizeObserver.observe(ref.current);
