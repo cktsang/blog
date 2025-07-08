4e5c1f9daeccb546b620ebf46eafb35a166387c9
cktsang <ck.tsang@hotmail.com>
2025-07-09 01:13:49 +0200

    Added dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { inter } from "./fonts/fonts";
 import Providers from "./providers";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
   title: {
@@ -19,14 +20,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} text-primary antialiased`}>
-        <div className="relative h-auto min-h-svh w-full md:space-y-4">
-          <Header />
-          <main>
-            <Providers>{children}</Providers>
-          </main>
-        </div>
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${inter.className} text-neutral-950 antialiased dark:text-neutral-50`}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative h-auto min-h-svh w-full md:space-y-4">
+            <Header />
+            <main>
+              <Providers>{children}</Providers>
+            </main>
+          </div>
+        </ThemeProvider>
       </body>
     </html>
   );
